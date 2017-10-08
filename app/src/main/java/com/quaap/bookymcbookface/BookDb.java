bd8f1a847406575492df7a1db7e48bf118a329df
quaap <kliethermes.tom@gmail.com>
2017-10-08 12:39:50 -0500

    analyse code

--- app/src/main/java/com/quaap/bookymcbookface/BookDb.java
+++ app/src/main/java/com/quaap/bookymcbookface/BookDb.java
@@ -6,7 +6,6 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.annotation.NonNull;
-import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
