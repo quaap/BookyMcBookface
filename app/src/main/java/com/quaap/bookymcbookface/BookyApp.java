5fcccc4e71e0eceeb816f4af9252ad671a26100d
quaap <kliethermes.tom@gmail.com>
2017-10-09 11:17:25 -0500

    headers and analyze code

--- app/src/main/java/com/quaap/bookymcbookface/BookyApp.java
+++ app/src/main/java/com/quaap/bookymcbookface/BookyApp.java
@@ -4,7 +4,16 @@ import android.app.Application;
 import android.content.Context;
 
 /**
- * Created by tom on 10/8/17.
+ * Copyright (C) 2017   Tom Kliethermes
+ *
+ * This file is part of BookyMcBookface and is is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
  */
 
 public class BookyApp extends Application {
--- app/src/main/java/com/quaap/bookymcbookface/GetBooksActivity.java
+++ app/src/main/java/com/quaap/bookymcbookface/GetBooksActivity.java
@@ -1,5 +1,6 @@
 package com.quaap.bookymcbookface;
 
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -19,6 +20,18 @@ import android.widget.Toast;
 
 import java.util.Map;
 
+/**
+ * Copyright (C) 2017   Tom Kliethermes
+ *
+ * This file is part of BookyMcBookface and is is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ */
 public class GetBooksActivity extends Activity implements View.OnClickListener, View.OnLongClickListener{
 
     EditText nameBox;
