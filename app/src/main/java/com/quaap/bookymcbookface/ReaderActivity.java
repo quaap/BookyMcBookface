047e3565b3e64d73d44871a789933aa0df35a831
quaap <kliethermes.tom@gmail.com>
2018-07-17 21:40:27 -0500

    fullscreen immersive flag

--- app/src/main/java/com/quaap/bookymcbookface/ReaderActivity.java
+++ app/src/main/java/com/quaap/bookymcbookface/ReaderActivity.java
@@ -478,7 +478,8 @@ public class ReaderActivity extends Activity {
                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE;
         decorView.setSystemUiVisibility(uiOptions);
     }
 
